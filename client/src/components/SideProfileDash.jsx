dc853640d74b6eb4481c7256fbc068027bee0c31
S Kartik <seshamkarthik3@gmail.com>
2025-06-20 20:52:50 +0530

    Profile update user Functionality

--- client/src/components/SideProfileDash.jsx
+++ client/src/components/SideProfileDash.jsx
@@ -1,45 +1,87 @@
-import { Avatar, Button, TextInput } from "flowbite-react";
-import { useSelector } from "react-redux";
-
+import { Alert, Avatar, Button, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice";
 
 export default function SideProfileDash() {
   const { currentUser } = useSelector((state) => state.user);
-  
+  const [formData, setFormData] = useState({});
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateuserError, setUpdateUserError] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserSuccess(null);
+    setUpdateUserError(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No Changes made");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated Successfully");
+      }
+    } catch (err) {
+      dispatch(updateFailure(err.message));
+      setUpdateUserError(err.message);
+    }
+  };
   return (
-    <div className="max-w-lg mx-auto w-full">
+    <div className="max-w-lg mx-auto w-[90%]">
       <h1 className="my-7 font-semibold text-3xl text-center">Profile</h1>
-      <form className="flex flex-col gap-4">
-        
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <div
           className="w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden"
           onClick={() => fileRef.current.click()}
         >
-          <Avatar alt="User" rounded
+          <Avatar
+            alt="User"
+            rounded
             className="rounded-full w-full h-full border-8 border-gray-300"
           />
         </div>
 
-        <div className="text-red-700 flex justify-between items-center">
-          <span className="cursor-pointer">Delect Account</span>
-          <span className="cursor-pointer">Sign Out</span>
-        </div>
         <TextInput
           placeholder="username"
           type="text"
           id="username"
           defaultValue={currentUser ? currentUser.username : "UserName"}
+          onChange={handleChange}
         />
         <TextInput
           placeholder="email"
           type="email"
           id="email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
         <TextInput
           placeholder="password"
           type="password"
           id="password"
-          defaultValue="password"
+          defaultValue=""
+          onChange={handleChange}
         />
         <Button
           type="submit"
@@ -49,6 +91,20 @@ export default function SideProfileDash() {
           Update Profile
         </Button>
       </form>
+      <div className="text-red-700 flex justify-between items-center mt-4">
+        <span className="cursor-pointer">Delect Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+      {updateUserSuccess && (
+        <Alert color="success" className="mt-5">
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateuserError && (
+        <Alert color="failure" className="mt-5">
+          {updateuserError}
+        </Alert>
+      )}
     </div>
   );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,9 +22,29 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signinFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signinFailure,
+  updateSuccess,
+  updateStart,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
